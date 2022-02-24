cb98d8d382030e07b9e366c776eea180a017ccf8
Volodymyr Petrytsya <vpetrytsia@gmail.com>
2022-02-23 22:44:32 -0500

    test data removed

--- tests/test1.js
+++ tests/test1.js
@@ -7,58 +7,7 @@ fixture `Testing API Request`
         .page`http://localhost:3001/`
 
 test("Test 1", async (t) => {
-  //    // expected test data
-  // const devicesToCompare = [
-  //   {
-  //     id: "Th3ngERn9",
-  //     system_name: "MAC-LEADER",
-  //     type: "MAC",
-  //     hdd_capacity: "2048",
-  //   },
-  //   {
-  //     id: "Q1JdBnE12",
-  //     system_name: "ARMANDO-SERVER",
-  //     type: "WINDOWS_SERVER",
-  //     hdd_capacity: "256",
-  //   },
-  //   {
-  //     id: "e7ocoQ2n3",
-  //     system_name: "MIGUEL-PC",
-  //     type: "WINDOWS_WORKSTATION",
-  //     hdd_capacity: "500",
-  //   },
-  //   {
-  //     id: "Jj5bn3G2H",
-  //     system_name: "FIRST-MAC",
-  //     type: "MAC",
-  //     hdd_capacity: "180",
-  //   },
-  //   {
-  //     id: "GT556QGnk",
-  //     system_name: "GOOD-MAC",
-  //     type: "MAC",
-  //     hdd_capacity: "500",
-  //   },
-  //   {
-  //     id: "ab1coL2n9",
-  //     system_name: "GILBERT-COMPUTER",
-  //     type: "WINDOWS_WORKSTATION",
-  //     hdd_capacity: "750",
-  //   },
-  //   {
-  //     id: "LM5dBnJ2G",
-  //     system_name: "MOON-SMART",
-  //     type: "WINDOWS_SERVER",
-  //     hdd_capacity: "256",
-  //   },
-  //   {
-  //     id: "Up5bcEQp4",
-  //     system_name: "JULIO-MAC-LOCAL",
-  //     type: "MAC",
-  //     hdd_capacity: "512",
-  //   },
-  // ];
-
+  
   // GET request for devices
   const getDevices = await axios.get('http://localhost:3000/devices');
   const res = getDevices.data;
